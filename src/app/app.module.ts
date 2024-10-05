0012b4cd95bb198eb0c028a5ce2878d6cdcbb079
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-10-06 00:45:14 +0200

    start with some new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
