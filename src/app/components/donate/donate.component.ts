a82dfe0b8e7be2a46cd3503e982c2e2eed1c4203
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-11-01 20:07:49 +0100

    new donation component

--- src/app/components/donate/donate.component.ts
+++ src/app/components/donate/donate.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html',
   styleUrl: './donate.component.css'
 })
-export class DonateComponent {
+export class DonateComponent implements OnInit {
 
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    // Load the Ko-fi script dynamically
+    const script = this.renderer.createElement('script');
+    script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+    script.onload = () => {
+      // Initialize the widget after the script is loaded
+      this.initKofiWidget();
+    };
+    this.renderer.appendChild(document.body, script);
+  }
+
+  private initKofiWidget(): void {
+    // Call the kofiWidgetOverlay.draw function to render the widget
+    (window as any).kofiWidgetOverlay.draw('suljevic', {
+      type: 'floating-chat',
+      'floating-chat.donateButton.text': 'Support me',
+      'floating-chat.donateButton.backgroundColor': '#f45d2',
+      'floating-chat.donateButton.textColor': '#fff'
+    });
+  }
 }
